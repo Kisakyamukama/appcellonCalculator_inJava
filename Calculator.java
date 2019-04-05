30d79a33dc581193b365192807ae6993e72b3dbf
Kabali Michael Kisakyamukama <kabalimichael03@gmail.com>
2019-04-05 22:27:23 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator{
 
     public static void main(String []args){
-        System.out.println("Appcellon Calculator ")
+        System.out.println("Appcellon Calculator ");
     }
 
 }
