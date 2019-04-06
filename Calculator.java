4fa778df28e50e57e34b5d44b714d93a4353c90d
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-06 09:31:43 +0300

    add

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,8 @@
 public class Calculator{
 
     public static void main(String []args){
-        System.out.println("Appcellon Calculator");
+        System.outln("Appcellon Calculator");
 
-    };
+    }
 
 }
