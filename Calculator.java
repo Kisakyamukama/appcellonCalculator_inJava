96904e863b4e5e69332110faba970b2f8d286969
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-06 09:21:46 +0300

    add

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator{
 
     public static void main(String []args){
-        System.out.println("Appcellon Calculator");
+        System.out.println("Appcellon Calculator")
 
         System
     }
